a9f81711955c0a96a87e3e87e09a00c5b9a579de
ajayhit <chaudharyhit@gmail.com>
2020-05-08 16:42:44 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstclass';
+  title = 'First Assignment';
+  first='';
+  second='';
+  action='';
+  result='';
+  onClickMe(action)
+  {
+    if(this.first!='' && this.second !='')
+    {
+        this.action=action;
+        if(action==='+')
+        {
+          this.result= String(parseInt(this.first)+parseInt(this.second));
+        }
+        if(action==='-')
+        {
+          this.result= String(parseInt(this.first)-parseInt(this.second));
+        }
+        if(action==='*')
+        {
+          this.result= String(parseInt(this.first)*parseInt(this.second));
+        }
+        if(action==='/')
+        {
+          this.result= String(parseInt(this.first)/parseInt(this.second));
+        }
+         if(action==='clear')
+        {
+          this.first='';
+          this.second='';
+          this.result='';
+          this.action='';
+        }
+    }
+    else
+    {
+      alert('enter any number');
+    }
+  }
 }
